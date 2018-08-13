68a1bc7d1e3b2334ec6138b4fc0e5a051e73c5c1
Hugo <hugovk@users.noreply.github.com>
2018-08-13 13:05:36 +0300

    Add script to list all the countries visited by a GPX track

---
+++ gpx_countries.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+"""
+List all the countries visited by a GPX track.
+
+python gpx_countries.py route.gpx
+"""
+import argparse
+import json
+
+import gpxpy  # # pip install gpxy
+import gpxpy.gpx
+from shapely.geometry import Point, shape  # pip install shapely
+from shapely.prepared import prep
+
+
+# https://stackoverflow.com/a/46589405/724176
+def get_country(lon, lat):
+    point = Point(lon, lat)
+    for country, geom in countries.items():
+        if geom.contains(point):
+            return country
+
+    return "unknown"
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("gpx", help="Input GPX file")
+    args = parser.parse_args()
+
+    print("Loading countries")
+    # https://github.com/datasets/geo-countries
+    with open("countries.geojson") as geojson_file:
+        data = json.load(geojson_file)
+
+    countries = {}
+    for feature in data["features"]:
+        geom = feature["geometry"]
+        country = feature["properties"]["ADMIN"]
+        countries[country] = prep(shape(geom))
+
+    print("{} countries loaded".format(len(countries)))
+
+    print("Loading GPX")
+    with open(args.gpx) as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+    print("Finding countries in GPX")
+    visited = set()
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                country = get_country(point.longitude, point.latitude)
+                if country not in visited:
+                    visited.add(country)
+                    print("{}. {}".format(len(visited), country))
