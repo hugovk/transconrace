da94050be403bac9216094763937f5eaf79b5a2d
Hugo <hugovk@users.noreply.github.com>
2018-08-28 11:58:44 +0300

    Process multiple GPX; show total/unique countries, borders crossed, and UTC times

--- gpx_countries.py
+++ gpx_countries.py
@@ -6,6 +6,7 @@ List all the countries visited by a GPX track.
 python gpx_countries.py route.gpx
 """
 import argparse
+import glob
 import json
 
 import gpxpy  # # pip install gpxy lxml
@@ -24,12 +25,53 @@ def get_country(lon, lat):
     return "unknown"
 
 
+def process_gpx(filename):
+    print("Loading GPX: {}".format(filename))
+    with open(filename) as gpx_file:
+        gpx = gpxpy.parse(gpx_file)
+
+    print("Finding countries in GPX")
+    visited = []
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                country = get_country(point.longitude, point.latitude)
+                if len(visited) == 0 or country != visited[-1]:
+                    new = country not in visited
+                    marker = "*" if new else ""
+                    visited.append(country)
+                    if args.unique:
+                        if new:
+
+                            print(
+                                "{:2d}. {} {}".format(
+                                    len(set(visited)), point.time, country
+                                )
+                            )
+                    else:
+                        print(
+                            "{:2d}. {:2d}. {} {}{}".format(
+                                len(visited),
+                                len(set(visited)),
+                                point.time,
+                                country,
+                                marker,
+                            )
+                        )
+    print("Unique country visits:\t{}".format(len(set(visited))))
+    print("Total country visits:\t{}".format(len(visited)))
+    print("Borders crossed:\t{}".format(len(visited) - 1))
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument("gpx", help="Input GPX file")
+    parser.add_argument("gpx", help="Input GPX files")
+    parser.add_argument(
+        "-u", "--unique", action="store_true", help="Only show unique countries"
+    )
     args = parser.parse_args()
 
     print("Loading countries")
@@ -45,18 +87,10 @@ if __name__ == "__main__":
         country = feature["properties"]["ADMIN"]
         countries[country] = prep(shape(geom))
 
-    print("{} countries loaded".format(len(countries)))
+    print("Countries loaded:\t{}".format(len(countries)))
 
-    print("Loading GPX")
-    with open(args.gpx) as gpx_file:
-        gpx = gpxpy.parse(gpx_file)
+    filenames = glob.glob(args.gpx)
+    print("GPX files found:\t{}".format(len(filenames)))
 
-    print("Finding countries in GPX")
-    visited = set()
-    for track in gpx.tracks:
-        for segment in track.segments:
-            for point in segment.points:
-                country = get_country(point.longitude, point.latitude)
-                if country not in visited:
-                    visited.add(country)
-                    print("{}. {}".format(len(visited), country))
+    for filename in filenames:
+        process_gpx(filename)
